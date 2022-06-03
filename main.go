f239340c2141d21b1de8dba6d0bd75ba627becd8
Shobhit0403 <f20180417@goa.bits-pilani.ac.in>
2022-06-03 19:42:52 +0530

    added all the functionalities

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -20,13 +23,71 @@ type Director struct {
 	Lastname  string `json:"lastname"`
 }
 
-var movies []int
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content=Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if params["id"] == item.ID {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// decoder Json -> object format
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(10000000))
+	movies = append(movies, movie)
+	// now we will encode object to json
+	json.NewEncoder(w).Encode(movies)
+
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if params["id"] == item.ID {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(10000000))
+			movies = append(movies, movie)
+			// now we will encode object to json
+			json.NewEncoder(w).Encode(movies)
+			return
+		}
+	}
+}
 
 func main() {
 	r := mux.NewRouter()
 
-	movies = append(movies)
-	movies = append(movies)
+	movies = append(movies, Movie{ID: "1", Isbn: "1234567", Title: "Movie one", Director: &Director{Firstname: "firstname", Lastname: "LastName"}})
+	// movies = append(movies)
 
 	//Defining Handle functions
 	r.HandleFunc("/movies", getMovies).Methods("GET")
